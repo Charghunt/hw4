190584ddbf8045c5e7350fde96b76f249e03d7f3
Charlotte Gunn Hunt <charlotte.gunn@kellogg.northwestern.edu>
2023-05-31 17:47:31 +0000

    added encryption file

---
+++ scripts/encryption.rb
@@ -0,0 +1,15 @@
+# to encrypt a secret, use BCrypt::Password.create(secret)
+# to test a secret, use BCrypt::Password.new(encrypted) == secret
+# run this file using rails runner scripts/encryption.rb
+
+# TODO:
+# 1. encrypt a secret and puts encrypted string
+secret = "tacos"
+encrypted_secret = BCrypt::Password.create(secret)
+
+
+# 2. prepare encrypted string for testing
+user_entered_secret = "tacos"
+result = BCrypt::Password.new(encrypted_secret) == user_entered_secret
+
+# 3. test secret against prepared encrypted string
