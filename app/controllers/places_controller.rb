7e67b9ce206d5ff74ee3ebd0fa03f36989daeaa7
Charlotte Gunn Hunt <charlotte.gunn@kellogg.northwestern.edu>
2023-06-01 16:20:59 +0000

    getting closer...

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -16,6 +16,7 @@ class PlacesController < ApplicationController
   def create
     @place = Place.new
     @place["name"] = params["place"]["name"]
+    @place["id"] = @current_user["id"]
     @place.save
     redirect_to "/places"
   end
